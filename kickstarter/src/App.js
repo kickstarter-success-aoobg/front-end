73abd87b6703c0b506ca23e28f8794b7e9c65c03
KSClopton <kyle.clopton@outlook.com>
2020-06-22 14:41:41 -0400

    Created react app, downloaded libraries, added components folder. Built AxiosAuth, and PrivateRouting to Login

---
+++ kickstarter/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import PrivateRoute from './Components/PrivateRoute'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+import SignUp from './Components/SignUp'
+import DashBoard from './Components/DashBoard'
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
