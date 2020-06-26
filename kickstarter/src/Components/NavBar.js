4b85f282b81dd62520ce55c715329b58fd6a523b
KSClopton <kyle.clopton@outlook.com>
2020-06-26 10:36:28 -0400

    Changed name of logo to NewLogo

--- kickstarter/src/Components/NavBar.js
+++ kickstarter/src/Components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
-import Logo from '../Images/Logo.PNG'
+import Logo from '../Images/NewLogo.PNG'
 
 const NavBar = () => {
  
